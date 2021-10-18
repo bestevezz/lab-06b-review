35c0d41268a30cdf2b47b28ecaaaa2d0ce0fb824
unknown <bryan.estevez@mail.citytech.cuny.edu>
2021-10-18 18:28:50 -0400

    I saved files all wrong, had to rewrite code for saytwice, shout, and temperature

--- Temperature.py
+++ Temperature.py
@@ -1,1 +1,5 @@
-importsys
+tempInCelsius = input("Pls enter a temperature in Celsius ")
+tempInFar = (float(tempInCelsius) * 1.8 + 32)
+tempInFar = round(tempInFar, 2)
+print(str(tempInCelsius) + " degrees Celsius is " +
+      str(tempInFar) + " degrees Fahrenheit.")
